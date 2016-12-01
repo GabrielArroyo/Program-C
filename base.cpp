da0a5f43aa1aef91134d09ab5c007e05b08b5460
GabrielArroyo <gabrielarroyo396@gmail.com>
2016-12-01 09:49:59 +0100

    Add files via upload

---
+++ base.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){	
+	int decimal;
+
+	printf ("Decimal: ");
+	scanf (" %i", &decimal);
+	//Hacer
+	//Imprimir decimal % 2
+	//Decimal = decimal / 2 
+	//Mientras decimal /2 > 0 
+	do{
+	printf ("%i", decimal % 2);
+	decimal /=2;
+	}while (decimal>0);
+
+	printf ("\n");
+	return EXIT_SUCCESS;
+}
