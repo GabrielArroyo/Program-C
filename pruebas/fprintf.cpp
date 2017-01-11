edcf79619d5d485b6c7959a52966e9973b479313
GabrielArroyo <gabrielarroyo396@gmail.com>
2017-01-11 13:07:14 +0100

    ejercicios de navidad

---
+++ pruebas/fprintf.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	FILE *tubo;
+
+
+	tubo = fopen("iberos.txt", "w");
+
+	printf("Hola\n");
+
+	fprintf(stdout, "Hola\n");
+	fprintf(tubo, "Hola\n");
+	fclose(tubo);
+
+	return EXIT_SUCCESS;
+}
--- pruebas/navidad/a_nivel_de_bits/1_nivel_bits.cpp
+++ pruebas/navidad/a_nivel_de_bits/1_nivel_bits.cpp
@@ -4,13 +4,17 @@
 int main(int argc, char *argv[]){
 
 
-	char decimal [];
+	int decimal [];
+	int binario [];
 	int resultado;
 
 	printf("Dame tu numero decimal: \n");
 	scanf(" %s", &decimal);
 
+	for (int i = 1; i = 0; i++){
+	
 
+	}
 
 
 	return EXIT_SUCCESS;
